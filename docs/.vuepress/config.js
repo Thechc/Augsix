8f3116a18a4720f06d1aa827c70971a714b9a386
thechc <734282231@qq.com>
2023-11-01 09:24:40 +0800

    commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    themeConfig: {
+        logo: '/assets/img/logo.jpg',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/about/' },
+            { text: 'External', link: 'https://google.com' },
+        ]
+    },
+
+}
